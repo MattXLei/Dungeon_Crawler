f3845eee47962d56772aa313e6c7485d5dd27632
Ivan_Wang <ivanwang1239@gmail.com>
2023-09-24 17:51:03 -0400

    add basic skeleton for enemy class

--- app/src/main/java/com/example/dungeoncrawler/Enemy.java
+++ app/src/main/java/com/example/dungeoncrawler/Enemy.java
@@ -1,4 +1,41 @@
 package com.example.dungeoncrawler;
 
 public class Enemy {
+    private static int health;
+    private static int speed;
+    private static int direction;
+    private static Location location;
+
+    public Enemy(int health, int speed, int direction, Location location) {
+        this.health = health;
+        this.speed = speed;
+        this.direction = direction;
+        this.location = new Location(0.0f, 0.0f);
+    }
+
+    public static int getHealth() {
+        return health;
+    }
+    public static void setHealth(int health) {
+        Enemy.health = health;
+    }
+    public static int getSpeed() {
+        return speed;
+    }
+    public static void setSpeed(int speed) {
+        Enemy.speed = speed;
+    }
+    public static int getDirection() {
+        return direction;
+    }
+    public static void setDirection(int direction) {
+        Enemy.direction = direction;
+    }
+    public static Location getLocation() {
+        return location;
+    }
+    public static void setLocation(float newX, float newY) {
+        Enemy.location.setxCord(newX);
+        Enemy.location.setyCord(newY);
+    }
 }

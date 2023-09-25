4750d08f956d8e95d922b84041a0f77572bd4662
Felix Wang <felix.wang@gatech.edu>
2023-09-24 22:10:38 -0400

    Changes

--- app/src/main/java/com/example/dungeoncrawler/WelcomeActivity.java
+++ app/src/main/java/com/example/dungeoncrawler/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.example.dungeoncrawler;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +16,11 @@ public class WelcomeActivity extends AppCompatActivity {
 
         // Starts app on click
         startBtn.setOnClickListener(v -> {
-            Intent start = new Intent(this, PreGameConfiguration.class);
-            startActivity(start);
+            Intent intent = new Intent(WelcomeActivity.this, PreGameConfiguration.class);
+            startActivity(intent);
         });
 
-        Button exitBtn = findViewById(R.id.exit); // change button resource
+        Button exitBtn = (Button) findViewById(R.id.exit); // change button resource
 
         // Exits app on click
         exitBtn.setOnClickListener(v -> {

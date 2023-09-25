6659560096030cec1f2d0e4ced2a393d0df0631d
MattXLei <thepetscompany123@gmail.com>
2023-09-24 22:17:07 -0400

    Add important functionality back

--- app/src/main/java/com/example/dungeoncrawler/GameActivity.java
+++ app/src/main/java/com/example/dungeoncrawler/GameActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 //import android.util.Log;
 import android.view.KeyEvent;
+//import android.widget.RelativeLayout;
+//import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 //import java.util.ArrayList;
@@ -16,73 +18,86 @@ import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity {
 
-
+    private float playerX;
+    private float playerY;
+    //private Random random = new Random();
+    //RelativeLayout gameLayout;
+    private int screenWidth;
+    private int screenHeight;
+    private Timer dotTimer;
     private double difficulty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gamescreen);
-
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_game);
+        screenWidth = getResources().getDisplayMetrics().widthPixels;
+        screenHeight = getResources().getDisplayMetrics().heightPixels;
+        // Spawn player in middle of screen
         playerX = screenWidth / 2;
         playerY = screenHeight / 2;
-        Button endScreenBtn = findViewById(R.id.endScreenButton); // change button resource
 
-        // Starts app on click
-        endScreenBtn.setOnClickListener(v -> {
-            Intent end = new Intent(this, EndActivity.class);
-            startActivity(end);
-        });
 
         // Get difficulty selected from Main screen.
         difficulty = getIntent().getDoubleExtra("difficulty", 1);
+        // Create dot list
+        // initializeDots();
+        // Draw dots on screen
+        // drawDots();
 
-        TextView textView = findViewById(R.id.healthText);
-        textView.append(" " +  ConfigureVar.getDifficulty());
-
-
-        TextView textView2 = findViewById(R.id.nameText);
-        textView2.append(Player.getName());
-
-        TextView textView3 = findViewById(R.id.healthText);
-        textView3.append("" + Player.getHealth());
-
+        /*
+        Timer to check every 0.5s
+         */
+        dotTimer = new Timer();
+        dotTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Loop for checks and assurances that need to run consistently
+                    }
+                });
+            }
+        }, 0, 500); // Check every .5 seconds
     }
-        // Handle key events to move the player
 
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        switch (keyCode) {
-//            case KeyEvent.KEYCODE_W:
-//                playerY -= 50;
-//                break;
-//            case KeyEvent.KEYCODE_A:
-//                playerX -= 50;
-//                break;
-//            case KeyEvent.KEYCODE_S:
-//                playerY += 50;
-//                break;
-//            case KeyEvent.KEYCODE_D:
-//                playerX += 50;
-//                break;
-//        }
-//        screenWidth = getResources().getDisplayMetrics().widthPixels;
-//        screenHeight = getResources().getDisplayMetrics().heightPixels;
-//        if (playerX < 0) {
-//            playerX = 0;
-//        }
-//        if (playerX > screenWidth) {
-//            playerX = screenWidth;
-//        }
-//        if (playerY < 0) {
-//            playerY = 0;
-//        }
-//        if (playerY > screenHeight) {
-//            playerY = screenHeight;
-//        }
-//        return true;
-//    }
+    // Handle key events to move the player
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_W:
+                playerY -= 50;
+                break;
+            case KeyEvent.KEYCODE_A:
+                playerX -= 50;
+                break;
+            case KeyEvent.KEYCODE_S:
+                playerY += 50;
+                break;
+            case KeyEvent.KEYCODE_D:
+                playerX += 50;
+                break;
+            default:
+                break;
+        }
+        screenWidth = getResources().getDisplayMetrics().widthPixels;
+        screenHeight = getResources().getDisplayMetrics().heightPixels;
+        if (playerX < 0) {
+            playerX = 0;
+        }
+        if (playerX > screenWidth) {
+            playerX = screenWidth;
+        }
+        if (playerY < 0) {
+            playerY = 0;
+        }
+        if (playerY > screenHeight) {
+            playerY = screenHeight;
+        }
+        return true;
+    }
 
     /*
     private void checkCollisions() {
@@ -107,27 +122,21 @@ public class GameActivity extends AppCompatActivity {
 
     /*
     Method that has logic to detect collisions.
+    private boolean isCollision(PlayerView playerView, Dot dot) {
+        float playerX = playerView.getX();
+        float playerY = playerView.getY();
+        int playerRadius = playerView.getRadius();
+        float dotX = dot.getX();
+        float dotY = dot.getY();
+        int dotRadius = dot.getRadius();
+        return playerRect.intersect(dotRect);
+    }
+     */
 
-    */
-//    private boolean isCollision(PlayerView playerView, Dot dot) {
-//        float playerX = playerView.getX();
-//        float playerY = playerView.getY();
-//        int playerRadius = playerView.getRadius();
-//        float dotX = dot.getX();
-//        float dotY = dot.getY();
-//        int dotRadius = dot.getRadius();
-//
-//            /*
-//            Creates a rectangle around dot, and checks for an intersection between player rect and
-//            dot rect. Intersection = collision.
-//             */
-//        RectF playerRect = new RectF(playerX - playerRadius, playerY - playerRadius, playerX + playerRadius, playerY + playerRadius);
-//        RectF dotRect = new RectF(dotX - dotRadius, dotY - dotRadius, dotX + dotRadius, dotY + dotRadius);
-//
-//        return playerRect.intersect(dotRect);
-//    }
-        //}
-
-
+    // Changes game screen to EndActivity
+    private void launchEndActivity() {
+        Intent intent = new Intent(this, EndActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
-

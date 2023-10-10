5c9e231736ba7d3808958c8f18bde352cd147975
Felix Wang <felix.wang@gatech.edu>
2023-10-10 19:21:54 -0400

    Unit test for sorted arrray

--- app/src/test/java/com/example/dungeoncrawler/LeaderboardsTest.java
+++ app/src/test/java/com/example/dungeoncrawler/LeaderboardsTest.java
@@ -11,10 +11,34 @@ public class LeaderboardsTest {
 
     Player player = Player.getPlayer();
     Leaderboard leaderboard = Leaderboard.getLeaderboard();
+    // Tests whether or not the correct score of the player shows up  on the leaderboard
     @Test
     public void addAttempt() {
+        Leaderboard.empty();
+        Player.setScore(100);
         Leaderboard.addAttempt();
-        assertEquals(Leaderboard.getAttempts()[0].getScore(),Player.getScore());
+        assertEquals(Leaderboard.getAttempts()[0].getScore(), Player.getScore());
+    }
+    // Test whether attempts automatically sorts
+    @Test
+    public void SortedAttempts() {
+        Leaderboard.empty();
+        Player.setScore(20);
+        Leaderboard.addAttempt();
+        Player.setScore(10);
+        Leaderboard.addAttempt();
+        Player.setScore(30);
+        Leaderboard.addAttempt();
+        Player.setScore(50);
+        Leaderboard.addAttempt();
+        Player.setScore(40);
+        Leaderboard.addAttempt();
+
+        assertEquals(Leaderboard.getAttempts()[0].getScore(), 10);
+        assertEquals(Leaderboard.getAttempts()[1].getScore(), 20);
+        assertEquals(Leaderboard.getAttempts()[2].getScore(), 30);
+        assertEquals(Leaderboard.getAttempts()[3].getScore(), 40);
+        assertEquals(Leaderboard.getAttempts()[4].getScore(), 50);
 
     }
 }

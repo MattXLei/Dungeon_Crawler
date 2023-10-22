429d6611c1726120d2f67db860d3945bdb83df52
Felix Wang <felix.wang@gatech.edu>
2023-10-22 19:07:30 -0400

    Movement test erros

--- app/src/test/java/com/example/dungeoncrawler/MovementTest.java
+++ app/src/test/java/com/example/dungeoncrawler/MovementTest.java
@@ -2,61 +2,63 @@ package com.example.dungeoncrawler;
 
 import static org.junit.Assert.assertEquals;
 
-import com.example.dungeoncrawler.model.Location;
+
 import com.example.dungeoncrawler.model.Player;
+
 import com.example.dungeoncrawler.model.RunStrategy;
 import com.example.dungeoncrawler.model.WalkStrategy;
-import com.example.dungeoncrawler.view.GameActivity;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class MovementTest {
+    
+    Player player = Player.getPlayer();
     @Test
     public void testWalk() {
         int x = 500;
         int y = 500;
-        Player.setLocation(x,y);
-        Player.setMovementStrategy(new WalkStrategy());
-        Player.getMovementStrategy().moveUp();
+        player.setLocation(x,y);
+        player.setMovementStrategy(new WalkStrategy());
+        player.getMovementStrategy().moveUp();
         y-=25;
-        assertEquals(Player.getLocation().getxCord(),x,0);
-        assertEquals(Player.getLocation().getyCord(),y,0);
-        Player.getMovementStrategy().moveDown();
+        assertEquals(player.getLocation().getxCord(),x,0);
+        assertEquals(player.getLocation().getyCord(),y,0);
+        player.getMovementStrategy().moveDown();
         y+=25;
-        assertEquals(Player.getLocation().getxCord(),x,0);
-        assertEquals(Player.getLocation().getyCord(),y,0);
-        Player.getMovementStrategy().moveLeft();
+        assertEquals(player.getLocation().getxCord(),x,0);
+        assertEquals(player.getLocation().getyCord(),y,0);
+        player.getMovementStrategy().moveLeft();
         x-=25;
-        assertEquals(Player.getLocation().getxCord(),x,0);
-        assertEquals(Player.getLocation().getyCord(),y,0);
-        Player.getMovementStrategy().moveRight();
+        assertEquals(player.getLocation().getxCord(),x,0);
+        assertEquals(player.getLocation().getyCord(),y,0);
+        player.getMovementStrategy().moveRight();
         x+=25;
-        assertEquals(Player.getLocation().getxCord(),x,0);
-        assertEquals(Player.getLocation().getyCord(),y,0);
+        assertEquals(player.getLocation().getxCord(),x,0);
+        assertEquals(player.getLocation().getyCord(),y,0);
     }
     @Test
     public void testRun() {
         int x = 500;
         int y = 500;
-        Player.setLocation(x,y);
-        Player.setMovementStrategy(new RunStrategy());
-        Player.getMovementStrategy().moveUp();
+        player.setLocation(x,y);
+        player.setMovementStrategy(new RunStrategy());
+        player.getMovementStrategy().moveUp();
         y-=50;
-        assertEquals(Player.getLocation().getxCord(),x,0);
-        assertEquals(Player.getLocation().getyCord(),y, 0);
-        Player.getMovementStrategy().moveDown();
+        assertEquals(player.getLocation().getxCord(),x,0);
+        assertEquals(player.getLocation().getyCord(),y, 0);
+        player.getMovementStrategy().moveDown();
         y+=50;
-        assertEquals(Player.getLocation().getxCord(),x,0);
-        assertEquals(Player.getLocation().getyCord(),y,0);
-        Player.getMovementStrategy().moveLeft();
+        assertEquals(player.getLocation().getxCord(),x,0);
+        assertEquals(player.getLocation().getyCord(),y,0);
+        player.getMovementStrategy().moveLeft();
         x-=50;
-        assertEquals(Player.getLocation().getxCord(),x,0);
-        assertEquals(Player.getLocation().getyCord(),y,0);
-        Player.getMovementStrategy().moveRight();
+        assertEquals(player.getLocation().getxCord(),x,0);
+        assertEquals(player.getLocation().getyCord(),y,0);
+        player.getMovementStrategy().moveRight();
         x+=50;
-        assertEquals(Player.getLocation().getxCord(),x,0);
-        assertEquals(Player.getLocation().getyCord(),y,0);
+        assertEquals(player.getLocation().getxCord(),x,0);
+        assertEquals(player.getLocation().getyCord(),y,0);
     }
 }

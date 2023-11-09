52e90856b7c4fcf0ff4fd2f75412808915459cc7
Felix Wang <felix.wang@gatech.edu>
2023-11-09 14:35:05 -0500

    Basic factory method

--- app/src/main/java/com/example/dungeoncrawler/model/Enemy2.java
+++ app/src/main/java/com/example/dungeoncrawler/model/Enemy2.java
@@ -1,6 +1,6 @@
 package com.example.dungeoncrawler.model;
 
-public class Enemy2 extends Enemy{
+public class Enemy2 extends  Enemy{
     public Enemy2() {
         super(100, 10, new Location(0,0));
     }
---
+++ app/src/main/java/com/example/dungeoncrawler/model/Spawner.java
@@ -0,0 +1,5 @@
+package com.example.dungeoncrawler.model;
+
+public abstract class Spawner {
+    public abstract Enemy createEnemy();
+}

2d90868c66d1f03934cd5d173cc222652d0e0f74
Ivan_Wang <ivanwang1239@gmail.com>
2023-11-21 21:47:36 -0500

    Add basic structure for speed decorator

--- app/src/main/java/com/example/dungeoncrawler/model/PowerupDecorator.java
+++ app/src/main/java/com/example/dungeoncrawler/model/PowerupDecorator.java
@@ -2,4 +2,8 @@ package com.example.dungeoncrawler.model;
 
 public abstract class PowerupDecorator {
     protected Powerup powerup;
+
+    public int getSpeed() {
+        return powerup.getSpeed();
+    }
 }

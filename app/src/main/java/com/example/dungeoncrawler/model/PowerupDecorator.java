85c5cf117b67308e039e41d39574449ee04f2748
flopwzh <phillipwzh@gmail.com>
2023-11-26 23:07:37 -0500

    add interface and corresponding methods for decorator

--- app/src/main/java/com/example/dungeoncrawler/model/PowerupDecorator.java
+++ app/src/main/java/com/example/dungeoncrawler/model/PowerupDecorator.java
@@ -1,7 +1,7 @@
 package com.example.dungeoncrawler.model;
 
-public abstract class PowerupDecorator {
-    protected Powerup powerup;
+public abstract class PowerupDecorator implements Powerupable {
+    protected Powerupable powerup;
 
     public int getSpeed() {
         return powerup.getSpeed();
